5137c5a1bb9cc3decb257da5ed6677e804ad487d
Jorge del Casar <jorge.casar@gmail.com>
2014-01-16 14:05:21 +0100

    Define User Model and include bcryp package

--- api/models/User.js
+++ api/models/User.js
@@ -8,12 +8,44 @@
 
 module.exports = {
 
-  attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
-  }
+	attributes: {
+		email: {
+			type: 'string',
+			required: true,
+			email: true,
+			unique: true
+		},
+		password: {
+			type: 'string',
+			required: true
+		},
+		// Override toJSON instance method to remove password value
+		toJSON: function() {
+			var obj = this.toObject();
+			delete obj.password;
+			return obj;
+		},
+		
+	},
+	// Lifecycle Callbacks
+	beforeCreate: function(values, next) {
+		hashPassword(values, next);
+	},
+	beforeUpdate: function(values, next) {
+		if(values.password) hashPassword(values, next);
+		else next();
+	}
 
 };
+
+var bcrypt = require('bcrypt');
+ 
+function hashPassword(values, next) {
+    bcrypt.hash(values.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        values.password = hash;
+        next();
+    });
+}
