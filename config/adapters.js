5f34185918b05b3648f14213f23ae32b88404262
Jorge del Casar <jorge.casar@gmail.com>
2014-01-16 11:56:06 +0100

    Config MongoDB as default adapter.

--- config/adapters.js
+++ config/adapters.js
@@ -14,26 +14,32 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'disk',
+	// If you leave the adapter config unspecified 
+	// in a model definition, 'default' will be used.
+	'default': 'disk',
 
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data is preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+	// Persistent adapter for DEVELOPMENT ONLY
+	// (data is preserved when the server shuts down)
+	disk: {
+		module: 'sails-disk'
+	},
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
+	// MySQL is the world's most popular relational database.
+	// Learn more: http://en.wikipedia.org/wiki/MySQL
+	myLocalMySQLDatabase: {
 
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
-  }
+		module: 'sails-mysql',
+		host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+		user: 'YOUR_MYSQL_USER',
+		// Psst.. You can put your password in config/local.js instead
+		// so you don't inadvertently push it up if you're using version control
+		password: 'YOUR_MYSQL_PASSWORD', 
+		database: 'YOUR_MYSQL_DB'
+	},
+
+	mongo: {
+		module: 'sails-mongo',
+		url: process.env.DB_URL,
+		schema: true
+	},
 };
